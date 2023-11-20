2fe1b6bb7f5f44c5b46ac67efe6b6aa67b5cb62d
qulamali7 <147071333+qulamali7@users.noreply.github.com>
2023-11-20 20:02:25 +0400

    add

---
+++ js tekrar/assets/js/script.js
@@ -0,0 +1,74 @@
+// const obj = {
+//   name: "Tony",
+//   surname: "Montana",
+//   year: 1983,
+// };
+
+// const { name, year } = obj;
+// console.log(name, year);
+
+// const copy={...obj}
+
+// const obj2 = { ...obj };
+
+// const arr = [13, 77, 777];
+// function Myforeach(arr, cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     cb(arr[i]);
+//   }
+// }
+// Myforeach(arr, (x) => console.log(x));
+
+// const arr = [13, 77, 777];
+// function MyMap(arr, cb) {
+//   newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     newArr.push(cb(arr[i]));
+//   }
+//   return newArr
+// }
+// console.log(MyMap(arr, element => element));
+
+// const arr = [13, 77, 777,88,66];
+// function MyFilter(arr, cb) {
+//   newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (cb(arr[i])) {
+//         newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr
+// }
+// console.log(MyFilter(arr, element => element%2===0 ? element:null));
+
+// const arr = [13, 77, 777, 88, 66];
+// function MyFindIndex(arr, cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (cb(arr[i])) {
+//       return i;
+//     }
+//   }
+// }
+// console.log(MyFindIndex(arr, (element) => (element % 2 === 0 ? element : null)));
+
+// const arr = [13, 77, 777,88,66];
+// function MyFind(arr, cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (cb(arr[i])) {
+//        return arr[i];
+//     }
+//   }
+// }
+// console.log(MyFind(arr, element => element%2===0 ? element:null));
+
+// const arr = [13, 77, 777,88,66];
+// function MyInclude(arr,item) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i]===item) {
+//        return true;
+//     }
+//   }
+//   return false
+// }
+// console.log(MyInclude(arr,15));
+console.log("9"+3);
